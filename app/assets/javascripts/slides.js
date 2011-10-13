dead7cfa7c377dca949d87692714abed462b86a8
Nick Meccia <nickmeccia@gmail.com>
2011-10-13 13:01:20 -0500

    lalalal

---
+++ app/assets/javascripts/slides.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+	
+	$('#slide2').hide();
+	$('#slide3').hide();
+	$('#slide4').hide();
+
+	
+	
+	$('#next1').click(function(){
+		$('#slide2').show("slide", { direction: "right" }, 1000);
+		$('#slide1').hide("slide", { direction: "left" }, 1000);
+	});
+	
+	
+	$('#prev2').click(function(){
+		$('#slide1').show("slide", { direction: "left" }, 1000);
+		$('#slide2').hide("slide", { direction: "right" }, 1000);
+	});
+	$('#next2').click(function(){
+		$('#slide3').show("slide", { direction: "right" }, 1000);
+		$('#slide2').hide("slide", { direction: "left" }, 1000);
+	});
+	
+	
+	$('#prev3').click(function(){
+		$('#slide2').show("slide", { direction: "left" }, 1000);
+		$('#slide3').hide("slide", { direction: "right" }, 1000);
+	});
+	$('#next3').click(function(){
+		$('#slide4').show("slide", { direction: "right" }, 1000);
+		$('#slide3').hide("slide", { direction: "left" }, 1000);
+	});
+	
+	
+	$('#prev4').click(function(){
+		$('#slide3').show("slide", { direction: "left" }, 1000);
+		$('#slide4').hide("slide", { direction: "right" }, 1000);
+	});
+	
+});
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Position::Application.config.secret_token = '4e1f78c432560b61dd77f32ac4015c83c2e9273a23d62a5a11a102badff1d1e03bcd6bcf5f1b61e3b8599a172b7e316d87ce91d4d06a673f35187527a7a57942'
