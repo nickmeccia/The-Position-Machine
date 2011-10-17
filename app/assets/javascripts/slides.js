8dccb14c31fccd73f4853e3368baaa8e8ec501fd
Nick Meccia <nickmeccia@gmail.com>
2011-10-17 17:39:43 -0500

    working on dem text boxers.

--- app/assets/javascripts/slides.js
+++ app/assets/javascripts/slides.js
@@ -5,7 +5,6 @@ $(document).ready(function(){
 	$('#slide4').hide();
 	
 	
-	
 	$('#next1').click(function(){
 		$('#slide2').show("slide", { direction: "right" }, 1000);
 		$('#slide1').hide("slide", { direction: "left" }, 1000);
@@ -37,5 +36,26 @@ $(document).ready(function(){
 		$('#slide4').hide("slide", { direction: "right" }, 1000);
 	});
 	
-
+	
+	
+	
+	
+	$('#top_hat_position').change(function() {
+		var str = $(this).children("option:selected").text();
+		$('#top_hat').css('position', str);
+	});
+	$('#top_hat_z-index').change(function() {
+		var str = $(this).children("option:selected").text();
+		$('#top_hat').css('z-index', str);
+	});
+	
+	$('#frame_position').change(function() {
+		var str = $(this).children("option:selected").text();
+		$('#frame').css('position', str);
+		$('#senator_edouard').css('position', str);
+	});
+	$('#frame_z-index').change(function() {
+		var str = $(this).children("option:selected").text();
+		$('#frame').css('z-index', str);
+	});
 });
